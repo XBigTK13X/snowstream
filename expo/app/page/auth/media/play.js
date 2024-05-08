7feae9dea5bbd9e9f7f57a6338277a762c68245b
XBigTK13X <xbigtk13x@gmail.com>
2024-05-08 14:18:51 -0500

    Integrating VLC

--- expo/app/page/auth/media/play.js
+++ expo/app/page/auth/media/play.js
@@ -3,14 +3,17 @@ import { Link } from "expo-router";
 import { View, Text, TouchableOpacity } from "react-native";
 import { Button, ListItem } from '@rneui/themed';
 import { VLCPlayer, VlCPlayerView } from 'react-native-vlc-media-player';
-
-
-
 import { useLocalSearchParams, useGlobalSearchParams } from 'expo-router';
 import { useSession } from '../../../auth-context';
 import { useSettings } from '../../../settings-context';
 
-export default function MovieShelfPage() {
+const calcVLCPlayerHeight = (windowWidth, aspetRatio) => {
+    return windowWidth * aspetRatio;
+};
+
+// https://github.com/razorRun/react-native-vlc-media-player/issues/186
+
+export default function PlayMediaPage() {
     const { signOut, apiClient } = useSession();
     const { routes } = useSettings();
     const localParams = useLocalSearchParams();
@@ -46,16 +49,37 @@ export default function MovieShelfPage() {
     })
     if (shelf && movie) {
         console.log({ web_path: movie.video_files[videoFileIndex].web_path })
-        return <VlCPlayerView
-            autoplay={true}
-            url={movie.video_files[videoFileIndex].web_path}
-            ggUrl=""
-            showGG={true}
-            showTitle={true}
-            title="Bee Movie"
-            showBack={true}
-            onLeftPress={() => { }}
-        />
+        const webPath = movie.video_files[videoFileIndex].web_path
+        return (
+            <>
+                <VLCPlayer
+                    source={{
+                        initType: 2,
+                        hwDecoderEnabled: 1,
+                        hwDecoderForced: 1,
+                        uri:
+                            webPath,
+                        initOptions: [
+                            '--no-audio',
+                            '--rtsp-tcp',
+                            '--network-caching=150',
+                            '--rtsp-caching=150',
+                            '--no-stats',
+                            '--tcp-caching=150',
+                            '--realrtsp-caching=150',
+                        ],
+                    }}
+                    autoplay={true}
+                    autoAspectRatio={true}
+                    resizeMode="cover"
+                    // videoAspectRatio={"4:3"}
+                    isLive={true}
+                    autoReloadLive={true}
+                    style={{ height: calcVLCPlayerHeight(Dimensions.get('window').width, 3 / 4), marginTop: 30 }}
+                />
+            </>
+        )
+
     }
     return (
         <Text>
