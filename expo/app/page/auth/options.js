9351afa3eb038106ee4e8634fec2f756b2b8682a
XBigTK13X <xbigtk13x@gmail.com>
2024-11-11 21:29:13 -0600

    Update button within the appo

--- expo/app/page/auth/options.js
+++ expo/app/page/auth/options.js
@@ -1,10 +1,23 @@
-import { View, Text } from "react-native";
-import { Link } from "expo-router";
+import { View, Text, Linking, Platform } from 'react-native'
+import { Button, ListItem } from '@rneui/themed'
 
 export default function OptionsPage() {
-  return (
-    <View>
-      <Text>These are the options.</Text>
-    </View>
-  );
+    let downloadButton = null
+    return (
+        <View>
+            <Text>These are the options.</Text>
+            <Button
+                title="Download Latest APK"
+                onPress={() => {
+                    if (Platform.isTV) {
+                        console.log('Download the TV app')
+                        Linking.openURL('http://9914.us:18081/snowstream-tv.apk')
+                    } else {
+                        console.log('Download the movile app')
+                        Linking.openURL('http://9914.us:18081/snowstream-mobile.apk')
+                    }
+                }}
+            />
+        </View>
+    )
 }
