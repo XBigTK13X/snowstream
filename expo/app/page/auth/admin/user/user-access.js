522569378ea02a8d6711c02d4155b932b65a1430
XBigTK13X <xbigtk13x@gmail.com>
2025-01-25 13:51:10 -0600

    Frontend toggles for user access

--- expo/app/page/auth/admin/user/user-access.js
+++ expo/app/page/auth/admin/user/user-access.js
@@ -5,9 +5,9 @@ export default function UserEditPage() {
     const { routes, config } = C.useSettings()
 
     const [userId, setUserId] = C.React.useState(null)
-    const [userTags, setUserTags] = C.React.useState('')
-    const [userShelves, setUserShelves] = C.React.useState('')
-    const [userStreamSources, setUserStreamSources] = C.React.useState('')
+    const [userTags, setUserTags] = C.React.useState([])
+    const [userShelves, setUserShelves] = C.React.useState([])
+    const [userStreamSources, setUserStreamSources] = C.React.useState([])
 
     const [tags, setTags] = C.React.useState('')
     const [shelves, setShelves] = C.React.useState('')
@@ -18,13 +18,20 @@ export default function UserEditPage() {
     C.React.useEffect(() => {
         if (!userId && localParams.userId) {
             apiClient.getUser(localParams.userId, true).then((response) => {
-                setUserTags(response.tags)
-                setUserShelves(response.shelves)
-                setUserStreamSources(response.streamSources)
+                if (response.tags) {
+                    setUserTags(response.tags)
+                }
+                if (response.shelves) {
+                    setUserShelves(response.shelves)
+                }
+                if (response.streamSources) {
+                    setUserStreamSources(response.streamSources)
+                }
+
             })
         }
         if (!tags) {
-            apiClient.getTags().then((tags) => {
+            apiClient.getTagList().then((tags) => {
                 setTags(tags)
             })
         }
@@ -50,13 +57,73 @@ export default function UserEditPage() {
     }
 
     const setShelfAccess = (shelfId, accessible) => {
-        console.log({ shelfId, accessible })
+        if (!accessible) {
+            const shelfIndex = userShelves.indexOf(shelfId)
+            if (shelfIndex !== -1) {
+                let moddedUserShelves = [...userShelves]
+                moddedUserShelves.splice(shelfIndex, 1)
+                setUserShelves(moddedUserShelves)
+            }
+        }
+        if (accessible) {
+            const shelfIndex = userShelves.indexOf(shelfId)
+            if (shelfIndex === -1) {
+                let modduedUserShelves = [...userShelves]
+                modduedUserShelves.push(shelfId)
+                setUserShelves(modduedUserShelves)
+            }
+        }
     }
+
+    const setStreamSourceAccess = (streamSourceId, accessible) => {
+        console.log({ streamSourceId, accessible })
+        if (!accessible) {
+            const streamSourceIndex = userStreamSources.indexOf(streamSourceId)
+            if (streamSourceIndex !== -1) {
+                let moddedStreamSources = [...userStreamSources]
+                moddedStreamSources.splice(streamSourceIndex, 1)
+                setUserStreamSources(moddedStreamSources)
+            }
+        }
+        if (accessible) {
+            const streamSourceIndex = userStreamSources.indexOf(streamSourceId)
+            if (streamSourceIndex === -1) {
+                let moddedStreamSources = [...userStreamSources]
+                moddedStreamSources.push(streamSourceId)
+                setUserStreamSources(moddedStreamSources)
+            }
+        }
+    }
+
+    const setTagAccess = (tagId, accessible) => {
+        if (!accessible) {
+            const tagIndex = userTags.indexOf(tagId)
+            if (tagIndex !== -1) {
+                let moddedUserTags = [...userTags]
+                moddedUserTags.splice(tagIndex, 1)
+                setUserTags(moddedUserTags)
+            }
+        }
+        if (accessible) {
+            const tagIndex = userTags.indexOf(tagId)
+            if (tagIndex === -1) {
+                let moddedUserTags = [...userTags]
+                moddedUserTags.push(tagId)
+                setUserTags(moddedUserTags)
+            }
+        }
+    }
+
+    console.log({
+        userShelves,
+        userTags,
+        userStreamSources
+    })
 
     let shelfPicker = null
     if (shelves && shelves.length) {
         const renderShelf = (shelf) => {
-            if (!userShelves || userShelves.indexOf(shelf.id) !== -1) {
+            if (userShelves && userShelves.indexOf(shelf.id) !== -1) {
                 return (
                     <C.SnowButton title={shelf.name + " YES"} onPress={() => { setShelfAccess(shelf.id, false) }}></C.SnowButton>
                 )
@@ -76,8 +143,9 @@ export default function UserEditPage() {
 
     let streamSourcePicker = null
     if (streamSources && streamSources.length) {
+        console.log({ userStreamSources })
         const renderStreamSource = (streamSource) => {
-            if (!userStreamSources || userStreamSources.indexOf(streamSource.id) !== -1) {
+            if (userStreamSources && userStreamSources.indexOf(streamSource.id) !== -1) {
                 return (
                     <C.SnowButton title={streamSource.name + " YES"} onPress={() => { setStreamSourceAccess(streamSource.id, false) }}></C.SnowButton>
                 )
@@ -97,7 +165,7 @@ export default function UserEditPage() {
     let tagPicker = null
     if (tags && tags.length) {
         const renderTag = (tag) => {
-            if (!userTags || userTags.indexOf(tag.id) !== -1) {
+            if (userTags && userTags.indexOf(tag.id) !== -1) {
                 return (
                     <C.SnowButton title={tag.name + " YES"} onPress={() => { setTagAccess(tag.id, false) }}></C.SnowButton>
                 )
