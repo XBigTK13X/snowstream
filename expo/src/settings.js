57e99dd5e827f203664089f591012bac9c0b2087
XBigTK13X <xbigtk13x@gmail.com>
2025-06-21 01:30:52 -0500

    Bump to the release version

--- expo/src/settings.js
+++ expo/src/settings.js
@@ -14,8 +14,8 @@ class Config {
             this.webApiUrl = this.beastWebApiUrl
         }
 
-        this.clientVersion = "0.11.2"
-        this.clientBuildDate = "June 20, 2025"
+        this.clientVersion = "0.11.3"
+        this.clientBuildDate = "June 21, 2025"
         this.clientDevBuildNumber = 1
         this.useNullVideoView = false
         this.debugVideoView = false
