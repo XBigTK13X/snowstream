e969384fb91019de5c582c283a9d3514855ed7c3
XBigTK13X <xbigtk13x@gmail.com>
2025-06-24 23:12:41 -0500

    Bugfix

--- script/full-release.sh
+++ script/full-release.sh
@@ -1,17 +1,16 @@
 #! /bin/bash
 
-echo "Building the container image"
-script/docker-build push
+echo "=-=- Building the container image -=-="
+script/docker-build.sh push
 
-echo "Running the latest version container on beast"
-ssh access@beast.9914.us "bash -c \"cd /mnt/docker; ./on/snowstream.sh"
+echo "=-=- Running the latest version container on beast -=-="
+ssh access@beast.9914.us "bash -c \"cd /mnt/docker; ./on/snowstream.sh\""
 
-echo "Build the apks"
+echo "=-=- Build the apks -=-="
 script/prod-generate-apks.sh
 
-echo "Push the apks up to the file server"
+echo "=-=- Push the apks up to the file server -=-="
 ~/script/push-apks.sh snowstream
 
-echo "Deploy the apks to all devices"
+echo "=-=- Deploy the apks to all devices -=-="
 ~/script/remote-adb.py
-
