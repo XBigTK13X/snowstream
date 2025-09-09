683cbbe2aebbe50d1942fd1c015ef4f11c4bfd28
XBigTK13X <xbigtk13x@gmail.com>
2025-09-09 01:40:30 -0500

    Working on the prebuild patch script

--- script/prebuild.py
+++ script/prebuild.py
@@ -8,6 +8,39 @@ with open('expo/android/build.gradle','r',encoding="utf-8") as read_handle:
             all_found = True
         if all_found and 'mavenCentral' in line:
             line += '\n    maven { url "/home/kretst/maven-repo" }\n'
+            all_found = False
         gradle_body += line
+
 with open('expo/android/build.gradle', 'w', encoding="utf-8") as write_handle:
+    write_handle.write(gradle_body)
+
+keystore = '''{
+            storeFile  file(keystoreProperties['KEYSTORE_PATH'])
+            storePassword  keystoreProperties['KEYSTORE_PASSWORD']
+            keyAlias keystoreProperties['KEY_ALIAS']
+            keyPassword keystoreProperties['KEY_PASSWORD']
+        }
+'''
+gradle_body = ''
+with open('expo/android/app/build.gradle','r',encoding="utf-8") as read_handle:
+    signing_found = False
+    for line in read_handle.readlines():
+        if 'def projectRoot' in line:
+            line += '''
+def keystorePropertiesFile = System.getenv('SNOWSTREAM_KEYSTORE_PROPS')
+def keystoreProperties = new Properties()
+keystoreProperties.load(new FileInputStream(keystorePropertiesFile))
+'''
+        if 'hermesCommand' in line:
+            line = '    hermesCommand = "/home/kretst/bin/hermes/hermesc"\n'
+
+        if 'minSdkVersion' in line:
+            line = '         minSdkVersion 26\n'
+
+        if signing_found and '}' in line:
+            line = f'        debug {keystore}\n        release {keystore}'
+            signing_found = False
+        gradle_body += line
+
+with open('expo/android/app/build.gradle', 'w', encoding="utf-8") as write_handle:
     write_handle.write(gradle_body)
