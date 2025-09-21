a66bd71cf27621dc11b34d4846256ce305ab9841
XBigTK13X <xbigtk13x@gmail.com>
2025-09-20 19:06:56 -0500

    Version bump

--- expo/src/settings.js
+++ expo/src/settings.js
@@ -4,8 +4,8 @@ class Config {
         this.stormWebApiUrl = 'http://192.168.101.30:8000' // Laptop
         this.beastWebApiUrl = 'http://beast.9914.us:9063' // Prod
 
-        this.clientVersion = "1.0.2"
-        this.clientBuildDate = "September 17, 2025"
+        this.clientVersion = "1.0.3"
+        this.clientBuildDate = "September 20, 2025"
         this.clientDevBuildNumber = 1
 
         this.debounceMilliseconds = 700
--- web-server/settings.py
+++ web-server/settings.py
@@ -4,8 +4,8 @@ import sys
 
 class Config:
     def __init__(self):
-        self.server_version = "1.0.2"
-        self.server_build_date = "September 17, 2025"
+        self.server_version = "1.0.3"
+        self.server_build_date = "September 20, 2025"
         self.server_build_dev_number = 1
         self.app_data_dir = '.snowstream/'
         self.display_config = None
