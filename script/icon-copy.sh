8ef42f77176d523b0a4d164e2005cf02498b13bc
XBigTK13X <xbigtk13x@gmail.com>
2025-09-24 12:23:50 -0500

    Bring over new appicons and latest snowui

---
+++ script/icon-copy.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+mkdir -p ./expo/assets/icon/android
+
+cp ~/develop/snowappicons/generated/snowstream/tvbanner.png expo/assets/icon/android/tvbanner.png
+
+cp ~/develop/snowappicons/generated/snowstream/appicon.png expo/assets/icon/appicon.png
+
+cp ~/develop/snowappicons/generated/snowstream/splash.png expo/assets/icon/splash.png
