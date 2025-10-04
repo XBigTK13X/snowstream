6a3eaa49a2b3f6d2dd0242331eabb33d2fba8ee2
XBigTK13X <xbigtk13x@gmail.com>
2025-10-04 18:52:13 -0500

    Version bump

--- expo/src/settings.js
+++ expo/src/settings.js
@@ -4,7 +4,7 @@ class Config {
         this.stormWebApiUrl = 'http://192.168.101.30:8000' // Laptop
         this.beastWebApiUrl = 'http://beast.9914.us:9063' // Prod
 
-        this.clientVersion = "1.1.1"
+        this.clientVersion = "1.1.2"
         this.clientBuildDate = "October 04, 2025"
         this.clientDevBuildNumber = 1
 
