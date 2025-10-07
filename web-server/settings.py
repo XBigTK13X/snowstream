00789dde71a1c3058a4e7c9a7aadd43fec48a40b
XBigTK13X <xbigtk13x@gmail.com>
2025-10-06 21:25:09 -0500

    New release

--- web-server/settings.py
+++ web-server/settings.py
@@ -4,8 +4,8 @@ import sys
 
 class Config:
     def __init__(self):
-        self.server_version = "1.1.7"
-        self.server_build_date = "October 05, 2025"
+        self.server_version = "1.1.8"
+        self.server_build_date = "October 06, 2025"
         self.server_build_dev_number = 1
         self.app_data_dir = '.snowstream/'
         self.display_config = None
