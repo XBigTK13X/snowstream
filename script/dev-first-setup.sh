51ecad6dc51f719b75cbb9dffd09ef315cc6181b
Lofty Snowman <xbigtk13x@gmail.com>
2026-02-16 10:41:42 -0600

    Update first time setup rules

--- script/dev-first-setup.sh
+++ script/dev-first-setup.sh
@@ -1,10 +1,23 @@
 #! /bin/bash
 
+sudo dnf install --allowerasing \
+    jc \
+    mediainfo \
+    ffmpeg \
+    java-21-openjdk-devel \
+    libxml2-devel \
+    libxslt-devel \
+    python3-devel \
+    libpq-devel \
+    python3.12 \
+    python3.12-devel
+
 cd web-server
-virtualenv -p /usr/bin/python3 venv
+python3.12 -m venv venv
 source venv/bin/activate
-pip install -r requirements.txt 
+pip install -r requirements.txt
 cd ..
-cd web-client
-npm install
+
+cd expo
+npx yarn install
 cd ..
