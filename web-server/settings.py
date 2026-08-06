f6633906a262d9251cbebf892dc4ed4a3406c93f
XBigTK13X <xbigtk13x@gmail.com>
2026-08-06 13:56:01 -0500

    Pull in expo-libmpv with latest mpv master and expo 57 fixes

--- web-server/settings.py
+++ web-server/settings.py
@@ -4,7 +4,7 @@ import sys
 
 class Config:
     def __init__(self):
-        self.server_version = "1.6.3"
+        self.server_version = "1.6.4"
         self.server_build_date = "August 06, 2026"
         self.server_build_dev_number = 1
         self.app_data_dir = '.snowstream/'
