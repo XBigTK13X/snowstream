01bc04dec4e54ec31f57f7569dca31ed5d74f8b5
XBigTK13X <xbigtk13x@gmail.com>
2026-08-16 06:38:48 -0500

    Ensure failing builds/scripts stop a deploymnet

--- script/full-release.sh
+++ script/full-release.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+set -e
+
 source script/variables.sh
 
 export NODE_ENV="production"
@@ -12,29 +14,26 @@ fi
 
 if [ "$MODE" == "server" ] || [ "$MODE" == "all" ]; then
 
-echo "=-=- Building the container image -=-="
-set -e
-script/docker-build.sh push
-set +e
+    echo "=-=- Building the container image -=-="
+    script/docker-build.sh push
 
-echo "=-=- Running the latest version container on beast -=-="
-ssh access@beast.9914.us "bash -c \"cd /mnt/docker; ./on/snowstream.sh\""
+    echo "=-=- Running the latest version container on beast -=-="
+    ssh access@beast.9914.us "bash -c \"cd /mnt/docker; ./on/snowstream.sh\""
 
 fi
 
 if [ "$MODE" == "client" ] || [ "$MODE" == "all" ]; then
 
-echo "=-=- Build the apks -=-="
-set -e
-script/prod-generate-apks.sh
-set +e
+    echo "=-=- Build the apks -=-="
+    script/prod-generate-apks.sh
 
-echo "=-=- Push the apks up to the file server -=-="
-~/script/push-apks.py snowstream
+    echo "=-=- Push the apks up to the file server -=-="
+    ~/script/push-apks.py snowstream
 
-echo "=-=- Deploy the apks to all devices -=-="
-~/script/remote-adb.py All deploy_snowstream
-
+    echo "=-=- Deploy the apks to all devices -=-="
+    ~/script/remote-adb.py All deploy_snowstream
 fi
 
 unset NODE_ENV
+
+set +e
